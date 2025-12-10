d18fddb3551dd674606a371057069a6540c9c05b
simonyansergey <sergey.a.simonyan@gmail.com>
2025-12-10 23:04:46 +0400

    feat: Oauth routes + methods

---
+++ app/Http/Controllers/Auth/Integrations/SpotifyAuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Integrations;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SpotifyAuthController extends Controller
+{
+    public function redirect()
+    {
+
+    }
+
+    public function callback()
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\Integrations\SpotifyAuthController;
+use App\Http\Controllers\Auth\Integrations\YoutubeAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(YoutubeAuthController::class)
+    ->prefix('/youtube')
+    ->middleware(['auth:sanctum'])
+    ->group(static function (): void {
+        Route::get('/redirect', 'redirect');
+        Route::post('/callback', 'callback');
+    });
+
+Route::controller(SpotifyAuthController::class)
+    ->prefix('/spotify')
+    ->middleware(['auth:sanctum'])
+    ->group(static function (): void {
+        Route::get('/redirect', 'redirect');
+        Route::post('/callback', 'callback');
+    });
