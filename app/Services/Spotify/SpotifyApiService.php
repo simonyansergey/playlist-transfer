3adf78de443dda3653c356a1da8218e35e4df9af
simonyansergey <sergey.a.simonyan@gmail.com>
2025-12-15 11:10:12 +0400

    feat: Add required methods (blank) for spotify service.

--- app/Services/Spotify/SpotifyApiService.php
+++ app/Services/Spotify/SpotifyApiService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Spotify;
 
+use App\Models\User;
 use App\Services\Oauth\SpotifyOauthService;
 
 class SpotifyApiService
@@ -9,4 +10,49 @@ class SpotifyApiService
     public function __construct(
         private readonly SpotifyOauthService $spotifyOauthService
     ) {}
+
+    /**
+     * Get current spotify user
+     *
+     * @param User $user
+     * @return string
+     */
+    public function getCurrentUser(User $user): string
+    {
+        return '';
+    }
+
+    /**
+     * @param User $user
+     * @param string $name
+     * @param string|null $visibility
+     * @return string
+     */
+    public function createPlaylist(User $user, string $name, ?string $visibility = 'public'): string
+    {
+        return '';
+    }
+
+    /**
+     * Get best matching track or null
+     *
+     * @param User $user
+     * @param string $searchQuery
+     * @return string|null
+     */
+    public function searchTrack(User $user, string $searchQuery): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @param User $user
+     * @param string $playlistId
+     * @param array $trackList
+     * @return string
+     */
+    public function addTracks(User $user, string $playlistId, array $trackList): string
+    {
+        return 'success';
+    }
 }
