8e3f71f7537963dd6eff01432d82d8f95ba89b17
simonyansergey <sergey.a.simonyan@gmail.com>
2025-12-15 17:38:14 +0400

    feat: Add empty transfer methods

--- app/Services/PlaylistTransfer/PlaylistTransferService.php
+++ app/Services/PlaylistTransfer/PlaylistTransferService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\PlaylistTransfer;
 
+use App\Models\User;
 use App\Services\Spotify\SpotifyApiService;
 use App\Services\Youtube\YoutubeApiService;
 
@@ -11,4 +12,29 @@ class PlaylistTransferService
         private readonly YoutubeApiService $youtubeApiService,
         private readonly SpotifyApiService $spotifyApiService
     ) {}
+
+    /**
+     * @param User $user
+     * @param string $playlistUrl
+     * @param array $options
+     * @return int
+     */
+    public function startTransfer(User $user, string $playlistUrl, array $options): int
+    {
+        return 0;
+    }
+
+    /**
+     * @param int $transferId
+     * @return array
+     */
+    public function executeTransfer(int $transferId): array
+    {
+        return [];
+    }
+
+    public function getTransfer(User $user, int $transferId): array
+    {
+        return [];
+    }
 }
